a0f332fa050a2807d5eaaf2055bee4d823935ac9
Francis Badasu <nytro04@gmail.com>
2020-07-29 21:56:32 +0000

    initial setup

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <Login />
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    Login: () => import('~/components/Auth/Login')
+  }
+}
+</script>
