246a1811f7596537e86de02edef81fd6388bc7a8
Francis Badasu <nytro04@gmail.com>
2020-08-25 14:53:00 +0000

    feat: add register page to pages: will refactor

---
+++ pages/register.vue
@@ -0,0 +1,143 @@
+<template>
+  <!-- todo: Refactor this to use single component for both register and login  -->
+  <v-app id="inspire" class="login">
+    <v-main>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
+            <v-card class="elevation-12">
+              <v-toolbar color="primary" dark flat>
+                <v-toolbar-title>Login form</v-toolbar-title>
+                <v-spacer></v-spacer>
+              </v-toolbar>
+              <v-card-text>
+                <v-form ref="form" v-model="isValid">
+                  <v-text-field
+                    v-model="userInfo.name"
+                    label="Name"
+                    name="name"
+                    prepend-icon="mdi-account"
+                    type="text"
+                    :rules="nameRules"
+                    required
+                  ></v-text-field>
+
+                  <v-text-field
+                    v-model="userInfo.email"
+                    label="Email"
+                    name="email"
+                    prepend-icon="mdi-account"
+                    type="email"
+                    :rules="emailRules"
+                    required
+                  ></v-text-field>
+
+                  <v-text-field
+                    id="password"
+                    v-model="userInfo.password"
+                    label="Password"
+                    name="password"
+                    prepend-icon="mdi-lock"
+                    type="password"
+                    :rules="passwordRules"
+                    required
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  :disabled="!isValid"
+                  color="primary"
+                  @click.prevent="registerUser"
+                  >Login</v-btn
+                >
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  // props: {
+  //   loginUser
+  // },
+  data() {
+    return {
+      // this.$refs.form.reset to reset form after form submission
+      isValid: true,
+      showPassword: false,
+      isLoading: false,
+      userInfo: {
+        name: '',
+        email: '',
+        password: ''
+      },
+      nameRules: [
+        (v) => !!v || 'Name is required',
+        (v) =>
+          (v && v.length > 3) || 'Name must be more than three(3) character'
+      ],
+      emailRules: [
+        (v) => !!v || 'Email is required',
+        (v) =>
+          /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
+          'Please provide a valid email'
+      ],
+      passwordRules: [
+        (v) => !!v || 'Password is required',
+        (v) =>
+          (v && v.length >= 8) ||
+          'Password must be eight(8) or more characters',
+        (v) =>
+          /(?=.*[A-Z])/.test(v) ||
+          'Password must have at least one(1) uppercase letter',
+        (v) =>
+          /(?=.*\d)/.test(v) || 'Password must have at least one(1) number',
+        (v) =>
+          /([!@#$%^&*])/.test(v) ||
+          'Password must have at least one(1) special character eg. !@#$%^&*'
+      ]
+    }
+  },
+  methods: {
+    async registerUser() {
+      try {
+        this.isLoading = true
+        await this.$axios.post('/users/register', this.userInfo)
+        const res = await this.$auth.loginWith('local', {
+          data: this.userInfo
+        })
+        this.isLoading = false
+        const user = res.data.data.user
+        this.$auth.setUser(user)
+
+        // console.log('User ==', this.$auth.user)
+        // this.$refs.form.reset()
+      } catch (errors) {
+        this.isLoading = false
+
+        // console.log(errors)
+      }
+    }
+  },
+  head() {
+    return {
+      title: 'Register',
+      // titleTemplate: `%s ${this.email} - Guard sys`,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'custom description'
+        }
+      ]
+    }
+  },
+  layout: 'login'
+}
+</script>
